357fa6357e0bc00927ef455536e3e9daa710b037
E-Hosny <ebrahimhosny511@gmail.com>
2024-01-18 04:42:32 +0300

    sec commit

--- index.js
+++
@@ -1,9 +0,0 @@
-$('.owl-carousel').owlCarousel({
-    items:2,
-    loop:false,
-    center:true,
-    margin:10,
-    URLhashListener:true,
-    autoplayHoverPause:true,
-    startPosition: 'URLHash'
-});
